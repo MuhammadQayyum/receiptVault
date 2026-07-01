fdd6e70bc8742a4951eb9e3da6debd5deeb1decd
Muhammad Qayyum <msqgrewal123@gmail.com>
2026-06-30 22:27:09 -0500

    Set timezone to Central Time

--- src/main/java/com/receiptvault/ReceiptvaultApplication.java
+++ src/main/java/com/receiptvault/ReceiptvaultApplication.java
@@ -3,10 +3,18 @@ package com.receiptvault;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import java.util.TimeZone;
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableScheduling
 public class ReceiptvaultApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Chicago"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ReceiptvaultApplication.class, args);
     }
